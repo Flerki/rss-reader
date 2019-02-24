5f9a1f4dfb10076a9b88b4c1dd1e29ae4471026f
Andrei <mairovichaa@yandex.ru>
2019-02-24 16:17:56 +0300

    Add tests using FeedStubServer

--- src/test/java/com/amairovi/service/FeedLoaderServiceTest.java
+++ src/test/java/com/amairovi/service/FeedLoaderServiceTest.java
@@ -1,17 +1,36 @@
 package com.amairovi.service;
 
 import com.amairovi.model.Feed;
+import com.amairovi.utility.FeedStubServer;
+import com.amairovi.utility.Main;
 import com.rometools.rome.feed.synd.SyndFeed;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.junit.jupiter.api.*;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeedLoaderServiceTest {
     private FeedLoaderService feedLoaderService;
+
+    private static FeedStubServer feedStubServer;
+    private static int port = 8080;
+
+    @BeforeAll
+    static void beforeAll() {
+        feedStubServer = new FeedStubServer(port);
+        feedStubServer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        feedStubServer.stop();
+    }
 
     @BeforeEach
     void setup() {
@@ -39,6 +58,38 @@ class FeedLoaderServiceTest {
 
             assertFalse(syndFeed.isPresent());
         }
+
+        @Test
+        void when_atom_returns_then_load_and_parse_successfully() throws IOException, FeedException {
+            Feed feed = new Feed();
+            feed.setHref(feedStubServer.getAtomUrl());
+
+            Optional<SyndFeed> result = feedLoaderService.load(feed);
+
+            SyndFeed expected = loadFeedFromFile("atom.xml");
+            assertTrue(result.isPresent());
+            assertEquals(expected, result.get());
+        }
+
+        @Test
+        void when_rss_returns_then_load_and_parse_successfully() throws IOException, FeedException {
+            Feed feed = new Feed();
+            feed.setHref(feedStubServer.getRssUrl());
+
+            Optional<SyndFeed> result = feedLoaderService.load(feed);
+
+            SyndFeed expected = loadFeedFromFile("rss.xml");
+            assertTrue(result.isPresent());
+            assertEquals(expected, result.get());
+        }
+
+        private SyndFeed loadFeedFromFile(String name) throws IOException, FeedException {
+            URL url = Main.class.getResource(name);
+            try (XmlReader xmlReader = new XmlReader(url)) {
+                return new SyndFeedInput().build(xmlReader);
+            }
+        }
+
     }
 
 }
