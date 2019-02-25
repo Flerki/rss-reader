9f9dc3425b2be809e4b0e6a25b344242f70850d7
Andrei <mairovichaa@yandex.ru>
2019-02-25 08:58:05 +0300

    Implement storing of feed params

--- src/main/java/com/amairovi/service/LoadTaskFactory.java
+++ src/main/java/com/amairovi/service/LoadTaskFactory.java
@@ -3,7 +3,9 @@ package com.amairovi.service;
 import com.amairovi.model.Feed;
 import com.amairovi.model.FeedFile;
 import com.amairovi.model.FeedProperties;
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
+import org.jdom2.Element;
 
 import java.util.Optional;
 import java.util.logging.Level;
@@ -47,6 +49,36 @@ public class LoadTaskFactory {
     private void updateFeed(SyndFeed syndFeed, Feed feed) {
         FeedProperties feedProperties = new FeedProperties(syndFeed);
         feed.setFeedProperties(feedProperties);
+
+        for (SyndEntry entry : syndFeed.getEntries()) {
+            checkEntryParameterPresenseAndAddIfNecessary("title", entry.getTitle(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("author", entry.getAuthor(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("comments", entry.getComments(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("link", entry.getLink(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("description", entry.getDescription(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("publishedDate", entry.getPublishedDate(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("updatedDate", entry.getUpdatedDate(), feed);
+            checkEntryParameterPresenseAndAddIfNecessary("uri", entry.getUri(), feed);
+
+            if (!entry.getContents().isEmpty()){
+                feed.addParameter("content");
+            }
+
+            if (!entry.getCategories().isEmpty()){
+                feed.addParameter("categories");
+            }
+
+            entry.getForeignMarkup()
+                    .stream()
+                    .map(Element::getName)
+                    .forEach(feed::addParameter);
+
+        }
     }
 
+    private void checkEntryParameterPresenseAndAddIfNecessary(String name, Object param, Feed feed) {
+        if (param != null) {
+            feed.addParameter(name);
+        }
+    }
 }
