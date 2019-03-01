c8e7775f3ba4e76e82eee5dc52d177c855460867
Andrei <mairovichaa@yandex.ru>
2019-03-01 13:00:57 +0300

    Create InvalidLinkFormatException

---
+++ src/main/java/com/amairovi/exception/InvalidLinkFormatException.java
@@ -0,0 +1,7 @@
+package com.amairovi.exception;
+
+public class InvalidLinkFormatException extends RuntimeException {
+    public InvalidLinkFormatException(Throwable cause) {
+        super(cause);
+    }
+}
