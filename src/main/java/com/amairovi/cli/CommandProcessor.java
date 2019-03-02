95e4b4d66d768f1c848853f1b2c54d543e747fb0
Andrei <mairovichaa@yandex.ru>
2019-03-02 10:11:56 +0300

    Add throwUnknownCommandException to CommandProcessor

--- src/main/java/com/amairovi/cli/CommandProcessor.java
+++ src/main/java/com/amairovi/cli/CommandProcessor.java
@@ -1,7 +1,17 @@
 package com.amairovi.cli;
 
+import com.amairovi.exception.UnknownCommandException;
+
+import static java.lang.String.join;
+
 public interface CommandProcessor {
-    default void process(String[] params){
+    default void process(String[] params) {
         throw new RuntimeException("not implemented");
+    }
+
+    default void throwUnknownCommandException(String[] params) {
+        String message = join(" ", params);
+
+        throw new UnknownCommandException("Unknown command: " + message);
     }
 }
