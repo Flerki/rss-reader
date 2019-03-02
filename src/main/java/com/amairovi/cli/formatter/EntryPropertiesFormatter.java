c922061341e7b05f43f343127e2bac25e1422521
Andrei <mairovichaa@yandex.ru>
2019-03-02 15:14:28 +0300

    Create EntryPropertiesFormatter

---
+++ src/main/java/com/amairovi/cli/formatter/EntryPropertiesFormatter.java
@@ -0,0 +1,25 @@
+package com.amairovi.cli.formatter;
+
+import java.util.Map;
+
+import static java.lang.System.lineSeparator;
+
+public class EntryPropertiesFormatter {
+    private final static String PATTERN = "%s: %s%n";
+
+    public String format(Map<String, Boolean> entryParameterNameToVisibility) {
+        if (entryParameterNameToVisibility.isEmpty()){
+            return "There is no property.";
+        }
+        StringBuilder str = new StringBuilder();
+        str.append("Feed's entries' properties")
+                .append(lineSeparator());
+
+        entryParameterNameToVisibility.keySet()
+                .stream()
+                .sorted()
+                .map(propertyName -> String.format(PATTERN, propertyName, entryParameterNameToVisibility.get(propertyName) ? "show" : "hide"))
+                .forEach(str::append);
+        return str.toString();
+    }
+}
