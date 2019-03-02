146465eed86f9c3b373a87f27dc7a449566e1477
Andrei <mairovichaa@yandex.ru>
2019-03-02 10:56:24 +0300

    Add support for filename and item-amount

--- src/main/java/com/amairovi/cli/command/SetProcessor.java
+++ src/main/java/com/amairovi/cli/command/SetProcessor.java
@@ -13,14 +13,39 @@ public class SetProcessor implements CommandProcessor {
 
     @Override
     public void process(String[] params) {
+        if (params.length != 4) {
+            throwUnknownCommandException(params);
+        }
+
         String property = params[1];
 
-        if (property.equals("poll-period")){
+        if (property.equals("poll-period")) {
             long pollPeriodInSec = Integer.valueOf(params[2]);
             int id = Integer.valueOf(params[3]);
             core.setPollPeriodInMs(id, pollPeriodInSec * 1000);
             return;
         }
 
+        if (property.equals("filename")) {
+            String filename = params[2];
+            int id = Integer.valueOf(params[3]);
+            core.setFeedFilename(id, filename);
+            return;
+        }
+
+        if (property.equals("item-amount")) {
+            int itemAmount = Integer.valueOf(params[2]);
+            int id = Integer.valueOf(params[3]);
+            core.setFeedAmountOfElementsAtOnce(id, itemAmount);
+            return;
+        }
+
+        throwUnknownCommandException(params);
+    }
+
+    @Override
+    public String commandFormat() {
+        return "set [poll-period|filename|item-amount] <poll-period-in-sec|filename|item-amount> <feedId>";
     }
 }
+
