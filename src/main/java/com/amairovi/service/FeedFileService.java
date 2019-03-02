581590d91fc7c3ce9800cfccca400c4bfc00514c
Andrei <mairovichaa@yandex.ru>
2019-03-02 10:54:21 +0300

    Remove unnecessary newline FeedFileService

--- src/main/java/com/amairovi/service/FeedFileService.java
+++ src/main/java/com/amairovi/service/FeedFileService.java
@@ -41,7 +41,6 @@ public class FeedFileService {
         synchronized (monitor) {
             try (BufferedWriter out = Files.newBufferedWriter(pathToFile, StandardOpenOption.APPEND)) {
                 out.write(data);
-                out.newLine();
             }
         }
     }
