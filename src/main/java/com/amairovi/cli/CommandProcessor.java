3a11bb6c3c8c7d1bafb8cd14a355510536305463
Andrei <mairovichaa@yandex.ru>
2019-03-02 10:20:15 +0300

    Add commandFormat to CommandProcessor

--- src/main/java/com/amairovi/cli/CommandProcessor.java
+++ src/main/java/com/amairovi/cli/CommandProcessor.java
@@ -5,13 +5,18 @@ import com.amairovi.exception.UnknownCommandException;
 import static java.lang.String.join;
 
 public interface CommandProcessor {
+
     default void process(String[] params) {
         throw new RuntimeException("not implemented");
     }
 
     default void throwUnknownCommandException(String[] params) {
-        String message = join(" ", params);
+        String actual = join(" ", params);
 
-        throw new UnknownCommandException("Unknown command: " + message);
+        throw new UnknownCommandException(actual, commandFormat());
+    }
+
+    default String commandFormat(){
+        return "Not implemented";
     }
 }
