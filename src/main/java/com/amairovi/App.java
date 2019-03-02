161e993aefc8f079ace10763a2716b892529f112
Andrei <mairovichaa@yandex.ru>
2019-03-02 21:26:08 +0300

    Create separate file for logs

--- src/main/java/com/amairovi/App.java
+++ src/main/java/com/amairovi/App.java
@@ -3,13 +3,33 @@ package com.amairovi;
 import com.amairovi.cli.Cli;
 import com.amairovi.core.FeedServiceFacade;
 
-import java.util.logging.LogManager;
+import java.io.IOException;
+import java.util.logging.*;
 
 public class App {
-    public static void main(String[] args) {
-        LogManager logManager = LogManager.getLogManager();
-        logManager.reset();
+    public static void main(String[] args) throws IOException {
+        setupLogging();
+
         FeedServiceFacade feedServiceFacade = new FeedServiceFacade();
-        new Cli(feedServiceFacade).start();
+        feedServiceFacade.initialize();
+
+        Cli cli = new Cli(feedServiceFacade);
+        cli.start();
+    }
+
+    private static void setupLogging() throws IOException {
+        Logger rootLogger = Logger.getLogger("");
+        rootLogger.setLevel(Level.ALL);
+
+        Handler[] handlers = rootLogger.getHandlers();
+        if (handlers[0] instanceof ConsoleHandler) {
+            rootLogger.removeHandler(handlers[0]);
+        }
+
+        FileHandler fileTxt = new FileHandler("logs.txt", true);
+
+        SimpleFormatter formatterTxt = new SimpleFormatter();
+        fileTxt.setFormatter(formatterTxt);
+        rootLogger.addHandler(fileTxt);
     }
 }
